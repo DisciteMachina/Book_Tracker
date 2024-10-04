ad29dfbfa28e6b7b9a8f429a0334afe074276400
chris <chrishicks.w@gmail.com>
2024-10-03 22:25:38 -0500

    Got averagePages and getCost methods working in PrintedBook

--- src/BookApp.java
+++ src/BookApp.java
@@ -1,3 +1,15 @@
-public class BookApp {
+// For printed books: title, author, genre, cost, pages
 
+// For audio books: title, author, genre, cost, length
+
+import java.text.DecimalFormat;
+
+public class BookApp {
+    public static void main(String[] args) {
+        PrintedBook book1 = new PrintedBook("Title1", "author1", "genre1", 10.0, 210);
+        PrintedBook book2 = new PrintedBook("Title2", "author2", "genre2", 20.0, 320);
+        PrintedBook book3 = new PrintedBook("Title3", "author3", "genre3", 30.0, 450);
+
+        System.out.println("Average cost of all books: " + PrintedBook.averagePages());
+    }
 }
--- src/PrintedBook.java
+++ src/PrintedBook.java
@@ -1,20 +1,46 @@
-public class PrintedBook extends BookAbstract {
-    protected int pages;
-    final private double COST_PER_PAGE = 10.0;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
-    public PrintedBook(String title, String author, String genre, double cost) {
-        super(title, author, genre, cost);
+public class PrintedBook extends BookAbstract {
+    final double COST_PER_PAGE = 10.0;
+
+    private static final ArrayList<PrintedBook> bookList = new ArrayList<>();
+    private static int totalPages;
+
+    public PrintedBook(String title, String author, String genre, double cost, int pages) {
+        super(title, author, genre, pages);
+        this.cost = pages * COST_PER_PAGE;
+        bookList.add(this);
+        totalPages += pages;
     }
 
     public double getCost() {
-        return 0;
+        return cost;
     }
 
-    public double averagePage() {
-        return 0;
+    public static String averagePages() {
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        // Avoid division by 0
+        if (bookList.isEmpty()) {
+            return "0.0";
+        }
+        
+        double average = (double) totalPages / bookList.size();
+        return df.format(average);
     }
 
-    public String lastThreePrintedBooks() {
-        return "";
+    public void lastThreePrintedBooks() {
+        ArrayList<PrintedBook> lastThree = new ArrayList<>();
+        int i = Math.max(0, bookList.size() - 3);
+        for (int j = i; j < bookList.size(); j ++) {
+            lastThree.add(bookList.get(j));
+        }
+        getDetails(lastThree);
     }
+
+    private void getDetails(ArrayList<PrintedBook> lastThree) {
+        String details = "Title: " + title;
+    }
+
 }
