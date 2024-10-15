0c1a65a2451428945d986e9c9af6d18836b87c16
chris <chrishicks.w@gmail.com>
2024-10-14 20:07:09 -0500

    sometimes git just randomly kills itself and forces me to completely redo my entire fucking tree

--- src/BookGUI.java
+++ src/BookGUI.java
@@ -165,3 +165,4 @@ class BookGUI extends JFrame {
         setVisible(true);
     }
 }
+
--- src/PrintedBook.java
+++ src/PrintedBook.java
@@ -1,5 +1,3 @@
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PrintedBook extends Book {
